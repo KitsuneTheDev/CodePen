f9350e993cf81297b5c368c08a4c26ff7ee8d8e0
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-02-01 22:22:13 +0300

    Update refresh.service.js

--- backend/src/service/refresh.service.js
+++ backend/src/service/refresh.service.js
@@ -3,21 +3,25 @@ import { saveRefreshToken } from "../repository/user.repository.js";
 import { AuthenticationError, ConflictError } from "../util/ErrorClient.util.js"
 import { createAccessToken, createRefreshToken, verifyRefreshToken } from "../util/jwt.util.js"
 
-export const refreshAccessToken = ({refreshToken}) => {
+export const refreshAccessToken = async ({refreshToken}) => {
     if(!refreshToken) {
         throw new AuthenticationError('No refresh token found');
     }
 
-    const userIdInToken = verifyRefreshToken(refreshToken);
-    const userIdInDatabase = getUserIdByRefreshToken(refreshToken) || null;
+    const userId = verifyRefreshToken(refreshToken);
+    const userIdFromDatabase = await getUserIdByRefreshToken(refreshToken);
 
-    if(userIdInToken !== userIdInDatabase) {
-        throw new ConflictError('token user id and db user id is not matched');
+    if(!userIdFromDatabase || userIdInToken !== userIdFromDatabase) {
+        throw new ConflictError('Token mismatch or invalid session');
     }
 
     const newRefreshToken = createRefreshToken(userId);
     const savedTokenUserId = saveRefreshToken({token: newRefreshToken, userId});
     const newAccessToken = createAccessToken(userId);
 
-    return {refreshToken: newRefreshToken, accessToken: newAccessToken, userId: savedTokenUserId};
+    return {
+        refreshToken: newRefreshToken,
+        accessToken: newAccessToken,
+        userId: savedTokenUserId
+    };
 }
