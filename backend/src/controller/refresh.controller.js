f9fa723577db606e5a0fe880728775c6f0ba98a2
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-02-07 21:38:21 +0300

    Update refresh.controller.js

--- backend/src/controller/refresh.controller.js
+++ backend/src/controller/refresh.controller.js
@@ -1,5 +1,19 @@
+import { refreshAccessToken } from "../service/refresh.service";
+
 export const refresh = (req, res, next) => {
     
+    try{
+        const refreshToken = req.cookies?.refreshToken?.token;
+        const newTokens = refreshAccessToken({refreshToken});
+        res.cookies('refreshToken', newTokens.newRefreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 1_000 * 60 * 60 * 24 * 7,
+        });
+    } catch(error) {
+        next(error);
+    }
     res.status(200).json({
         message: req.cookies?.refreshToken?.token
     });
