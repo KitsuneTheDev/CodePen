f27fe52147d7faf9dc5a782fcdc8317d5d259d41
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-03-12 23:27:35 +0300

    update
    - context
    - app.js

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -1,6 +1,15 @@
 import style from './Main.module.css';
+import { useAuth } from '../../contexts/authContext.jsx';
+import { useEffect } from 'react';
 
 export default function Main() {
+
+    const context = useAuth()
+
+    useEffect(() => {
+        console.log(context);
+    }, []);
+
     return(
         <h1>MAIN</h1>
     );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './contexts/authContext.jsx'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
