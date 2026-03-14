b2f3e0f8cce49bfa75a24eaf7fb6c9380f378a80
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-03-14 23:18:16 +0300

    Update Main.jsx

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -1,16 +1,16 @@
-import style from './Main.module.css';
-import { useAuth } from '../../contexts/authContext.jsx';
-import { useEffect } from 'react';
+// import style from './Main.module.css';
+// import { useAuth } from '../../contexts/authContext.jsx';
+// import { useEffect } from 'react';
 
-export default function Main() {
+// export default function Main() {
 
-    const context = useAuth()
+//     const context = useAuth()
 
-    useEffect(() => {
-        console.log(context);
-    }, []);
+//     useEffect(() => {
+//         console.log(context);
+//     }, []);
 
-    return(
-        <h1>MAIN</h1>
-    );
-}
+//     return(
+//         <h1>MAIN</h1>
+//     );
+// }
